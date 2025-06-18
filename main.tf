1d5753c593f6192f3ec3dd2a557050939d5b9038
venu222m <145119205+venu222m@users.noreply.github.com>
2025-06-18 16:09:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "5.50.0"
+   }
+ }
+}
+
+
+provider "aws" {
+ region = "us-east-1"
+
+
+resource "aws_vpc" "this" {
+ cidr_block = "10.30.0.0/16"
+}
